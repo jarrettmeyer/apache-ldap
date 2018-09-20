d0670395fe86a8c2fd291e6ed66a2f2d1a611abd
Jarrett Meyer <meyer_jarrett@network.lilly.com>
2018-09-20 13:48:29 -0400

    deploying 2018-09-20 13:48:28

--- deploy.ps1
+++ deploy.ps1
@@ -1,9 +1,11 @@
 $commitMessage = "deploying " + (Get-Date -f "yyyy-MM-dd HH:mm:ss")
 git add --all
+Write-Host
+Write-Host "Creating new commit: $commitMessage..."
 git commit -m "$commitMessage"
 Write-Host
-Write-Host "Commit Message: $commitMessage"
-Write-Host
+Write-Host "Pushing changes to origin/master..."
 git push origin master
 Write-Host
+Write-Host "Starting new build..."
 oc start-build demo --follow
