44569ac6fd5b7576a703aa3b00d7ffd2b64b1118
Jarrett Meyer <meyer_jarrett@network.lilly.com>
2018-09-20 13:43:14 -0400

    deploying 2018-09-20 13:43:13

--- deploy.ps1
+++ deploy.ps1
@@ -1,5 +1,4 @@
-$time = Get-Date
-$commitMessage = "deploying " + $time -f "yyyy-MM-dd HH:mm:ss"
+$commitMessage = "deploying " + (Get-Date -f "yyyy-MM-dd HH:mm:ss")
 git add --all
 git commit -m "$commitMessage"
 git push origin master
